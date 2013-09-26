bb565e7522c269bfd52650aeef35fbb82f909812
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-09-26 15:39:50 -0700

    Fixed js to respect X's O's

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var turn = 2;
 function playIt(){
-	if (event.target.className != 'played') {
+	if (event.target.className.split(" ")[3] != "played") {
 		if (turn % 2 == 0) {
-			event.target.innerHTML = "<a>X</a>";
 			event.target.classList.add('played');
+			event.target.innerHTML = "X";
 		}
 		else {
-			event.target.innerHTML = "<a>O</a>";
 			event.target.classList.add('played');
+			event.target.innerHTML = "O";
 		}
 		turn++;
 	}
@@ -16,9 +16,15 @@ function playIt(){
 	}
 }
 
+// function played() {
+// 	if (event.target.getElementsByClassName('cell')){
+// 		event.target.classList.add('played');
+// 	}
+// }
 
 
-for(c=1;c<=3;++c)
-	for(r=1;r<=3;++r)
-		alert(
-			document.getElementById("cell"+r+"_"+c).innerHTML);
+
+// for(c=1;c<=3;++c)
+// 	for(r=1;r<=3;++r)
+// 		alert(
+// 			document.getElementById("cell"+r+"_"+c).innerHTML);
