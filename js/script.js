6b4c06ebafbe88dba345607895f85936fd04177a
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-09-26 16:50:00 -0700

    Added some logic and an array :)

--- js/script.js
+++ js/script.js
@@ -16,15 +16,20 @@ function playIt(){
 	}
 }
 
-// function played() {
-// 	if (event.target.getElementsByClassName('cell')){
-// 		event.target.classList.add('played');
-// 	}
-// }
 
-
-
-// for(c=1;c<=3;++c)
-// 	for(r=1;r<=3;++r)
-// 		alert(
-// 			document.getElementById("cell"+r+"_"+c).innerHTML);
+function navCells(){
+	var cellArray = [ [null, null, null], [null, null, null], [null, null, null] ];
+	for(c=0;c<=2;++c){
+		for(r=0;r<=2;++r){
+			cellArray[c][r] = document.getElementById("cell"+c+"_"+r).innerHTML;
+		}
+	}
+	for(x=0; x<=2; ++x){
+		if(cellArray[0][x] == cellArray[1][x] && cellArray[1][x] == cellArray[2][x] && cellArray[0][x] != "<a class=\"placeholder\">.</a>"){
+			alert(cellArray[0][x] + "WON!");
+		}
+		if(cellArray[x][0] == cellArray[x][1] && cellArray[x][1] == cellArray[x][2] && cellArray[x][0] != "<a class=\"placeholder\">.</a>"){
+			alert(cellArray[x][0] + "WON!");
+		}
+	}
+}
