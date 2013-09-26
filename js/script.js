6b498bffd7c70857a3c2a8c2693b8d2b7189f862
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-09-26 14:49:54 -0700

    Added some sweet js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+var turn = 2;
+function playIt(){
+	if (event.target.className != 'played') {
+		if (turn % 2 == 0) {
+			event.target.innerHTML = "<a>X</a>";
+			event.target.classList.add('played');
+		}
+		else {
+			event.target.innerHTML = "<a>O</a>";
+			event.target.classList.add('played');
+		}
+		turn++;
+	}
+	else {
+		console.log('Choose somewhere else, punk');
+	}
+}
+
+
+
 for(c=1;c<=3;++c)
 	for(r=1;r<=3;++r)
 		alert(
