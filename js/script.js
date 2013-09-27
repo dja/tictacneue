2bf18e9d8b4fc05f16f3e3855a89c0cc8e7b67e4
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-09-26 21:56:50 -0700

    Added Stop Game Feature

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 var turn = 2;
+var playedCells = 0;
+var alreadyWon = false;
+
 function playIt(){
-	if (event.target.className.split(" ")[3] != "played") {
+	if (alreadyWon == false && event.target.className.split(" ")[3] != "played") {
 		if (turn % 2 == 0) {
 			event.target.classList.add('played');
+			playedCells++;
 			event.target.innerHTML = "X";
 		}
 		else {
 			event.target.classList.add('played');
+			playedCells++;
 			event.target.innerHTML = "O";
 		}
 		turn++;
+		navCells();
 	}
 	else {
-		console.log('Choose somewhere else, punk');
+		console.log('Nice try, punk');
 	}
 }
-
 
 function navCells(){
 	var cellArray = [ [null, null, null], [null, null, null], [null, null, null] ];
@@ -26,10 +31,43 @@ function navCells(){
 	}
 	for(x=0; x<=2; ++x){
 		if(cellArray[0][x] == cellArray[1][x] && cellArray[1][x] == cellArray[2][x] && cellArray[0][x] != "&nbsp;"){
-			alert(cellArray[0][x] + "WON!");
+			toggleVisibility('playagain');
+			alreadyWon = true;
+			alert(cellArray[0][x] + " WON!");
 		}
 		if(cellArray[x][0] == cellArray[x][1] && cellArray[x][1] == cellArray[x][2] && cellArray[x][0] != "&nbsp;"){
-			alert(cellArray[x][0] + "WON!");
+			toggleVisibility('playagain');
+			alreadyWon = true;
+			alert(cellArray[x][0] + " WON!");
 		}
+	}
+	if(cellArray[0][0] == cellArray[1][1] && cellArray[1][1] == cellArray[2][2] && cellArray[0][0] != "&nbsp;" ){
+		toggleVisibility('playagain');
+		alreadyWon = true;
+		alert(cellArray[0][0] + " WON!");
+	}
+	if(cellArray[2][0] == cellArray[1][1] && cellArray[1][1] == cellArray[0][2] && cellArray[2][0] != "&nbsp;" ){
+		toggleVisibility('playagain');
+		alreadyWon = true;
+		alert(cellArray[2][0] + " WON!");
+	}
+	else if(playedCells == 9){
+		toggleVisibility('playagain');
+		alert("No one won.");
+	}
+}
+
+function switchVisibility(id1, id2){
+	document.getElementById(id1).style.visibility = 'hidden';
+	document.getElementById(id2).style.visibility = 'visible';
+}
+
+function toggleVisibility(id){
+	var vis = document.getElementById(id).style.visibility;
+	if( vis == 'visible'){
+		document.getElementById(id).style.visibility = 'hidden';
+	}
+	else {
+		document.getElementById(id).style.visibility = 'hidden';
 	}
 }
