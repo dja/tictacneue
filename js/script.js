b007eb5712d3614cf3d30210ab269acc8ff3ddd4
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-09-26 22:39:31 -0700

    Watching Greys Anatomy and upping sensitivity

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var alreadyWon = false;
 
 if (typeof window.DeviceMotionEvent != 'undefined') {
     // Shake sensitivity (a lower number is more)
-    var sensitivity = 40;
+    var sensitivity = 80;
 
     // Position variables
     var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
